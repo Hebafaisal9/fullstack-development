09b8a477f27a59e4ac91b763b231c2c88dec5458
hebafaisal9 <faisalheba9@gmail.com>
2026-01-09 22:26:52 +0300

    DAY8 TASK COMPLETED

---
+++ DAY8 TASK/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+  <h1>hello world</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, facilis!</p>
+    </>
+  )
+}
+
+export default App
